d3e003933b8988aa9225ac1ca9d97b15c003bf87
Pastenkopie <pastenkopie@proton.me>
2026-06-23 11:45:17 -0400

    changed blank tab name

--- editor/changes.ts
+++ editor/changes.ts
@@ -5334,7 +5334,7 @@ export class ChangeSongTitle extends Change {
 			}
 		}
 
-        document.title = newTitle !== "" ? newTitle : EditorConfig.versionDisplayName;
+        document.title = newTitle !== "" ? newTitle : "•";
 
         doc.notifier.changed();
         if (oldValue != newValue) this._didSomething();
